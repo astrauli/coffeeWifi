e4ea64383b9861f6d482aad002f49814f67e1621
Aaron <astrauli@gmail.com>
2017-12-18 13:28:43 -0800

    finish landing page

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let slides = document.querySelectorAll('#slides .slide');
+  let currentSlide = 0;
+  let slideInterval = setInterval(nextSlide,3000);
+
+  function nextSlide() {
+      slides[currentSlide].className = 'slide';
+      currentSlide = (currentSlide+1)%slides.length;
+      slides[currentSlide].className = 'slide showing';
+  }
+})
