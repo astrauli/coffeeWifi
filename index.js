8265a571daffd67f89d2fdf6983d861eabc18538
Aaron <astrauli@gmail.com>
2017-12-25 11:09:13 -0800

    add mobile responsive features

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ document.addEventListener("DOMContentLoaded", () => {
       currentSlide = (currentSlide+1)%slides.length;
       slides[currentSlide].className = 'slide showing';
   }
-})
+
+  if (window.screen.width <= 480) {
+    let menuList = document.getElementsByClassName("menu-list")[0];
+    let hamburger = document.getElementsByClassName("menu-list-container")[0];
+    let originalClasses = menuList.className;
+    hamburger.addEventListener('click', () => {
+      if (menuList.className.includes("hidden")) {
+        menuList.className = originalClasses;
+      } else {
+        menuList.className += " hidden"        
+      }
+    })
+  }
+
+});
